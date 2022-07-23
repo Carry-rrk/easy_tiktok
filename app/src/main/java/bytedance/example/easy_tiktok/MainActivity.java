61b1d834fc6d123dd4d4fd42e0a579c904a2ef0a
= <2578312909@qq.com>
2022-07-23 17:46:52 +0800

    CurrentProject

---
+++ app/src/main/java/bytedance/example/easy_tiktok/MainActivity.java
@@ -0,0 +1,14 @@
+package bytedance.example.easy_tiktok;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
