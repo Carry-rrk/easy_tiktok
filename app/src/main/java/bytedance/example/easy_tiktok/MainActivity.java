5fdd22db62941adeb14b46bb112744399993e3b1
范圣玺 <55611772+fanshengxi@users.noreply.github.com>
2022-07-24 15:02:26 +0800

    Update MainActivity.java

--- app/src/main/java/bytedance/example/easy_tiktok/MainActivity.java
+++ app/src/main/java/bytedance/example/easy_tiktok/MainActivity.java
@@ -10,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        //线程休眠1秒钟
+        Thread.sleep(1000);
+        
     }
 }
