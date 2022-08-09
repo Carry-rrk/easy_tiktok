a1fc23ca6a13f936cb3324ef32a9f94bce27b5bf
= <2578312909@qq.com>
2022-08-09 20:50:56 +0800

    Merge branch 'FsxBranch' of https://github.com/Carry-rrk/easy_tiktok into FsxBranch

--- app/src/main/java/bytedance/example/easy_tiktok/utils/MainVm.java
+++ app/src/main/java/bytedance/example/easy_tiktok/utils/MainVm.java
@@ -3,10 +3,8 @@ package bytedance.example.easy_tiktok.utils;
 import android.app.Application;
 
 import androidx.annotation.NonNull;
-import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.ViewModel;
 
-public class MainVm extends AndroidViewModel {
-    public MainVm(@NonNull Application application) {
-        super(application);
-    }
+public class MainVm extends ViewModel {
+
 }
