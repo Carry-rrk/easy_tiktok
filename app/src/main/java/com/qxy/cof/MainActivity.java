d66ae801c2eeea99713c4a5c2d75e37c5666a49a
carry-rrk <rruikai@gmail.com>
2022-08-11 14:21:14 +0800

    get  access_tkoen test

---
+++ app/src/main/java/com/qxy/cof/MainActivity.java
@@ -0,0 +1,63 @@
+package com.qxy.cof;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.bytedance.sdk.*;
+import com.bytedance.sdk.open.aweme.authorize.model.Authorization;
+import com.bytedance.sdk.open.douyin.DouYinOpenApiFactory;
+import com.bytedance.sdk.open.douyin.DouYinOpenConfig;
+import com.bytedance.sdk.open.douyin.api.DouYinOpenApi;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private String mScope = "user_info,trial.whitelist,discovery.ent";
+    DouYinOpenApi douYinOpenApi;;
+    private String clientkey ="awtpi7iykd02xini";
+    String[] mPermissionList = new String[] {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE};
+    static final int PHOTO_REQUEST_GALLERY = 10;
+    private ArrayList<String> mUri = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+//        Log.d("rrkdebug",""+(douYinOpenApi==null));
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button but = findViewById(R.id.button);
+        DouYinOpenApiFactory.init(new DouYinOpenConfig(clientkey));
+        douYinOpenApi = DouYinOpenApiFactory.create(this);
+//        Log.d("rrkdebug",""+(douYinOpenApi==null));
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendAuth();
+            }
+        });
+    }
+    private boolean sendAuth() {
+        Authorization.Request request = new Authorization.Request();
+        request.scope = mScope;                          // 用户授权时必选权限
+//        request.optionalScope0 = "user_info";
+//        request.optionalScope0 = "mobile";     // 用户授权时可选权限（默认选择）
+//        request.optionalScope0 = mOptionalScope1;    // 用户授权时可选权限（默认不选）
+        request.state = "ww";                                   // 用于保持请求和回调的状态，授权请求后原样带回给第三方。
+        Log.d("rrkdebug",""+(douYinOpenApi==null));
+        return douYinOpenApi.authorize(request);               // 优先使用抖音app进行授权，如果抖音app因版本或者其他原因无法授权，则使用wap页授权
+
+    }
+
+}
---
+++ app/src/main/java/com/qxy/cof/douyinapi/DouYinEntryActivity.java
@@ -0,0 +1,111 @@
+package com.qxy.cof.douyinapi;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Message;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+import com.bytedance.sdk.open.aweme.CommonConstants;
+import com.bytedance.sdk.open.aweme.authorize.model.Authorization;
+import com.bytedance.sdk.open.aweme.common.handler.IApiEventHandler;
+import com.bytedance.sdk.open.aweme.common.model.BaseReq;
+import com.bytedance.sdk.open.aweme.common.model.BaseResp;
+import com.bytedance.sdk.open.douyin.DouYinOpenApiFactory;
+import com.bytedance.sdk.open.douyin.api.DouYinOpenApi;
+import com.qxy.cof.MainActivity;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class DouYinEntryActivity extends Activity implements IApiEventHandler  {
+    DouYinOpenApi douYinOpenApi;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        douYinOpenApi = DouYinOpenApiFactory.create(this);
+        douYinOpenApi.handleIntent(getIntent(), this);
+    }
+
+    @Override
+    public void onReq(BaseReq req) {
+
+    }
+
+    @Override
+    public void onResp(BaseResp resp) {
+//        Log.d("rrkdebug","1111");
+        // 授权成功可以获得authCode
+        if (resp.getType() == CommonConstants.ModeType.SEND_AUTH_RESPONSE) {
+            Authorization.Response response = (Authorization.Response) resp;
+            if (resp.isSuccess()) {
+                String code = ((Authorization.Response) resp).authCode;
+                OkHttpClient httpClient = new OkHttpClient.Builder().build();
+                RequestBody rb = new FormBody.Builder()
+                        .add("client_secret","bd48ef702cfa7aef1f11d3ae7974e96c")
+                        .add("code",code)
+                        .add("grant_type","authorization_code")
+                        .add("client_key","awtpi7iykd02xini")
+                        .build();
+                Request  re = new Request.Builder()
+                        .url("https://open.douyin.com//oauth/access_token/")
+                        .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                        .post(rb)
+                        .build();
+                Call call = httpClient.newCall(re);
+                call.enqueue(new Callback() {
+                                 @Override
+                                 public void onFailure(Call call, IOException e) {
+                                     e.printStackTrace();
+                                     Log.d("rrkdebug", "response:falier");
+                                 }
+
+                                 @Override
+                                 public void onResponse(Call call, okhttp3.Response response) throws IOException {
+                                    String s = response.body().string();
+                                     String ac_token = null;
+                                     try {
+                                         ac_token = new JSONObject(s).getJSONObject("data").getString("access_token");
+                                     } catch (JSONException e) {
+                                         e.printStackTrace();
+                                     }
+                                     Log.d("rrkdebug", "response: " +ac_token );
+                                 }
+                             }
+                );
+
+//                Log.d("rrkdebug","jixule");
+                Toast.makeText(this, "授权成功，获得权限：" + response.grantedPermissions,
+                        Toast.LENGTH_LONG).show();
+
+            } else {
+                Toast.makeText(this, "授权失败" + response.grantedPermissions,
+                        Toast.LENGTH_LONG).show();
+            }
+
+        }
+    }
+
+    @Override
+    public void onErrorIntent(@Nullable Intent intent) {
+        // 错误数据
+        Toast.makeText(this, "intent出错啦", Toast.LENGTH_LONG).show();
+        finish();
+    }
+}
